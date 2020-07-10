56e57372ca70fcdbc155c3ce99d89772eeeb2f29
unknown <jethanhargrave@gmail.com>
2020-07-10 12:13:39 -0400

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,16 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return None
+    cache = {}
+    # weights: {weight: index}
+    for i in range(len(weights)):
+        cache[weights[i]] = i
+    # find match
+    for i in range(len(weights)):
+        equalToLimit = limit-weights[i]
+        if equalToLimit in cache:
+            return (max(i, cache[equalToLimit]), min(i, cache[equalToLimit]))
+
+
+if __name__ == "__main__":
+    test = [ 4, 6, 10, 15, 16 ]
+    print(get_indices_of_item_weights(test, 5, 21))
